534874b6b2fe82cddf44eb2dc8ddc5f624d735c6
David Asatrian <david.asatrian@gmail.com>
2023-02-19 00:55:35 -0700

    Add README, favicon, and fix routing for GitHub pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# [david-phx.github.io/portfolio](https://david-phx.github.io/portfolio/)
+
+![Deploy to GitHub Pages](https://github.com/david-phx/portfolio/actions/workflows/deploy.yml/badge.svg)
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/'
+  base: '/portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        '404.html': resolve(__dirname, '404.html')
+      }
+    }
+  }
 })
